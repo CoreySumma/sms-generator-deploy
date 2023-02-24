cd60aeb593b174ae9e996d498421c2056595e805
CoreySumma <CoreySumma@gmail.com>
2023-02-24 14:59:14 -0600

    drying code out for deployment

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import logo from "./restriction.png";
+import React from "react";
+import logo from "../../assets/restriction.png";
 import "./App.css";
-import SMSForm from "./SMSForm";
-import RelationForm from "./components/RelationForm";
+import SMSForm from "../../components/SMSForm/SMSForm";
+import RelationForm from "../../components/RelationForm/RelationForm";
 import { Configuration, OpenAIApi } from "openai";
 import { useState, useEffect } from "react";
 
@@ -11,7 +11,6 @@ export default function App() {
     apiKey: process.env.REACT_APP_OPEN_AI_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  const [input, setInput] = useState("");
   const [result, setResult] = useState("");
   const [relationData, setRelationData] = useState("");
 
@@ -25,7 +24,7 @@ export default function App() {
       prompt: `Please write me a text message to my ${relationData} that is funny, with a lot of dark humor, and a lot of sarcasm, that explains that I am busy.  Please add in something about how I need to call them later after I am done doing something completely unrealistic with sarcasm.  The last sentence should be a fact about something completely random and dark that makes no sense that has nothing to do with anything. Depending on the person I am asking you to send it to, please tailor it to them! Each time I ask you to write me a text, please produce something you have not said before.`,
       temperature: 0,
       max_tokens: 100,
-      temperature: .9,
+      temperature: 0.9,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
     });
@@ -33,15 +32,24 @@ export default function App() {
   };
 
   useEffect(() => {
-    console.log(result)
+    console.log(result);
   }, [result]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <RelationForm updateRelation={updateRelation} doStuff={doStuff} setResult={setResult} result={result}/>
-        <SMSForm doStuff={doStuff} result={result} updateRelation={updateRelation}/>
+        <RelationForm
+          updateRelation={updateRelation}
+          doStuff={doStuff}
+          setResult={setResult}
+          result={result}
+        />
+        <SMSForm
+          doStuff={doStuff}
+          result={result}
+          updateRelation={updateRelation}
+        />
       </header>
     </div>
   );
