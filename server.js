b6ded4a0527b59c046a088e13e3f243b6c758918
CoreySumma <CoreySumma@gmail.com>
2023-02-27 17:44:43 -0600

    deployment attempt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 require('dotenv').config();
 const client = require("twilio")(
   process.env.REACT_APP_TWILIO_ACCOUNT_SID,
