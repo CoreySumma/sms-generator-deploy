f1217a4806e2de426762aea8e81d9192768b0736
CoreySumma <CoreySumma@gmail.com>
2023-03-01 18:19:18 -0600

    update sever.js

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.listen(port, () =>
   console.log(`Express server is running on port ${port}`)
 );
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
