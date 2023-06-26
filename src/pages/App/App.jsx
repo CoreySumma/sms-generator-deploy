41cedd0b000ba1ba15b2994abc08cc781d8bf710
CoreySumma <CoreySumma@gmail.com>
2023-06-26 18:22:03 -0500

    test again

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,7 @@ import SMSForm from "../../components/SMSForm/SMSForm";
 import RelationForm from "../../components/RelationForm/RelationForm";
 import { Configuration, OpenAIApi } from "openai";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function App() {
   const configuration = new Configuration({
@@ -19,19 +20,30 @@ export default function App() {
   };
 
   const doStuff = async () => {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Without using quotation marks please write me a custom short text message to my ${relationData} that is funny, with a lot of dark humor and sarcasm, that explains that I am busy. Please add in something about how I need to call them later after I am done doing something completely unrealistic. The last sentence should be a true fact about something completely random that has nothing to do with anything. Depending on the person I am asking you to send it to, please tailor it to them! Each time I ask you to write me a text, please produce something you have not said before.`,
-      temperature: 0.7,
-      max_tokens: 100,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    setResult(response.data.choices[0].text.trim());
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/completions',
+        {
+          model: "text-davinci-003",
+          prompt: `Without using quotation marks please write me a custom short text message to my ${relationData} that is funny, with a lot of dark humor and sarcasm, that explains that I am busy. Please add in something about how I need to call them later after I am done doing something completely unrealistic. The last sentence should be a true fact about something completely random that has nothing to do with anything. Depending on the person I am asking you to send it to, please tailor it to them! Each time I ask you to write me a text, please produce something you have not said before.`,
+          temperature: 0.7,
+          max_tokens: 100,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0,
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_OPEN_AI_KEY}`,
+            'Content-Type': 'application/json',
+          }
+        }
+      );
+  
+      setResult(response.data.choices[0].text.trim());
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error);
+    }
   };
-
-  useEffect(() => {
-  }, [result]);
 
   return (
     <div className="App">
